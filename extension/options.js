0e9acc8ece9ef497e073d544293e8f6a349fc033
Gitnix <gitnix1337@gmail.com>
2017-09-14 00:45:15 -0400

    use mutation observer to get starting node

--- extension/options.js
+++ extension/options.js
@@ -1,29 +1,27 @@
 // Saves options to chrome.storage
 function save_options() {
-	const dayInput = document.getElementById("dayInput")
+	const dayInput = document.getElementById('dayInput')
 	const dayValue = Math.abs(dayInput.value)
 	if (isNaN(dayValue) || !Number.isInteger(dayValue)) {
 		return
 	}
 
 	chrome.storage.sync.get(
-		// default here shouldn't be needed
-		// but better than not declaring one I guess
 		{
-			csr_dayValues: [90, 180]
+			csr_dayValues: [90, 180],
 		},
 		function(obj) {
 			const newValues = [...obj.csr_dayValues, dayValue]
 
 			chrome.storage.sync.set(
 				{
-					csr_dayValues: newValues
+					csr_dayValues: newValues,
 				},
 				function() {
 					restoreLogic(newValues)
-				}
+				},
 			)
-		}
+		},
 	)
 }
 
@@ -31,8 +29,8 @@ function save_options() {
 // stored in chrome.storage.
 function restore_options() {
 	const defaults = [90, 180]
-	chrome.storage.sync.get("csr_dayValues", function(obj) {
-		if (!obj.hasOwnProperty("csr_dayValues") || obj.csr_dayValues.length < 1) {
+	chrome.storage.sync.get('csr_dayValues', function(obj) {
+		if (!obj.hasOwnProperty('csr_dayValues') || obj.csr_dayValues.length < 1) {
 			chrome.storage.sync.set({ csr_dayValues: defaults }, () => {
 				restoreLogic(defaults)
 			})
@@ -48,25 +46,25 @@ function restoreLogic(items) {
 }
 
 function updateRenderedList(items) {
-	const valueList = document.getElementById("values")
-	valueList.innerHTML = ""
+	const valueList = document.getElementById('values')
+	valueList.innerHTML = ''
 	items.forEach((value, index) =>
 		valueList.insertAdjacentHTML(
-			"beforeend",
-			`<li><a id="${index}"><i title="Remove Item" class="fa fa-times-circle" aria-hidden="true"></i></a>  ${value} days</li>`
-		)
+			'beforeend',
+			`<li><a id="${index}"><i title="Remove Item" class="fa fa-times-circle" aria-hidden="true"></i></a>  ${value} days</li>`,
+		),
 	)
 }
 
 function addEventListeners(items) {
 	items.forEach((val, index) => {
 		const el = document.getElementById(index)
-		el.addEventListener("click", () => deleteItem(index))
+		el.addEventListener('click', () => deleteItem(index))
 	})
 }
 
 function deleteItem(index) {
-	chrome.storage.sync.get("csr_dayValues", function(obj) {
+	chrome.storage.sync.get('csr_dayValues', function(obj) {
 		const newValues = inverseSplice(obj.csr_dayValues, index)
 		chrome.storage.sync.set({ csr_dayValues: newValues }, function() {
 			restore_options()
@@ -79,5 +77,5 @@ function inverseSplice(a, index) {
 	return [...a.slice(0, index), ...a.slice(index + 1, a.length)]
 }
 
-document.addEventListener("DOMContentLoaded", restore_options)
-document.getElementById("add").addEventListener("click", save_options)
+document.addEventListener('DOMContentLoaded', restore_options)
+document.getElementById('add').addEventListener('click', save_options)
--- extension/searchRanges.js
+++ extension/searchRanges.js
@@ -1,37 +1,34 @@
-setTimeout(initData, 800)
-
-function initData() {
+function initData(node) {
 	// default set if user has not visited options
 	chrome.storage.sync.get({ csr_dayValues: [90, 180] }, obj => {
-		const dateVals = obj.csr_dayValues
+		let dateVals = obj.csr_dayValues
 
-		const dates = dateVals.map(numDays => {
-			const d = new Date()
+		let dates = dateVals.map(numDays => {
+			let d = new Date()
 			d.setDate(d.getDate() - numDays)
 			return d
 		})
 
-		const dateObjects = dates.map(date => {
+		let dateObjects = dates.map(date => {
 			return {
 				year: date.getFullYear(),
 				month: date.getMonth() + 1,
-				day: date.getDate()
+				day: date.getDate(),
 			}
 		})
 
-		populateList(dateVals, dateObjects)
+		populateList(node, dateVals, dateObjects)
 	})
 }
 
-function populateList(dateVals, dateObjects) {
+function populateList(node, dateVals, dateObjects) {
 	dateObjects.forEach((curr, index) => {
-		const firstElement = document.getElementById("qdr_")
-		const parentElement = firstElement.parentNode
+		let parentElement = node.parentNode
 
-		const newElement = document.createElement("li")
+		let newElement = document.createElement('li')
 
-		newElement.setAttribute("class", "hdtbItm")
-		newElement.setAttribute("id", `qdr_custom_${index}`)
+		newElement.setAttribute('class', 'hdtbItm')
+		newElement.setAttribute('id', `qdr_custom_${index}`)
 
 		newElement.innerHTML = `<a class="q qs" href="${window.location
 			.href}&tbs=cdr%3A1%2Ccd_min%3A${curr.month}%2F${curr.day}%2F${curr.year}%2Ccd_max%3A&tbm=">Past ${dateVals[
@@ -41,3 +38,18 @@ function populateList(dateVals, dateObjects) {
 		parentElement.insertBefore(newElement, parentElement.firstChild)
 	})
 }
+
+let Observer = new MutationObserver((mutations, observer) => {
+	let hasRun = false
+	let index = 0
+	mutations.forEach(() => {
+		let insertionNode = document.getElementById('qdr_')
+		if (insertionNode && !hasRun) {
+			observer.disconnect()
+			hasRun = true
+			initData(insertionNode)
+		}
+	})
+})
+
+Observer.observe(document, { childList: true, subtree: true })
